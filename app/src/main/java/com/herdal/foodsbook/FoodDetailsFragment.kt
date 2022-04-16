54c4a6244fa4bce6ae12a8a06eb88ba7012fa493
herdal06 <h.erdal721@gmail.com>
2022-04-16 04:53:58 +0300

    Test navigation argument with buttons

--- app/src/main/java/com/herdal/foodsbook/FoodDetailsFragment.kt
+++ app/src/main/java/com/herdal/foodsbook/FoodDetailsFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_food_details.*
 
 class FoodDetailsFragment : Fragment() {
+
+    private var foodId = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +19,18 @@ class FoodDetailsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_food_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            foodId = FoodDetailsFragmentArgs.fromBundle(it).foodId // test
+        }
+
+        buttonFoodDetail.setOnClickListener {
+            val action = FoodDetailsFragmentDirections.actionFoodDetailsFragmentToFoodListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
--- app/src/main/java/com/herdal/foodsbook/FoodListFragment.kt
+++ app/src/main/java/com/herdal/foodsbook/FoodListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_food_list.*
 
 class FoodListFragment : Fragment() {
 
@@ -15,5 +17,14 @@ class FoodListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_food_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonFoodList.setOnClickListener {
+            val action = FoodListFragmentDirections.actionFoodListFragmentToFoodDetailsFragment(7)
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
