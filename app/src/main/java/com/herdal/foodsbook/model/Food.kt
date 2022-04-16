b9d90c6b35613ca1c3e98fb66184187f76e3ba1f
herdal06 <h.erdal721@gmail.com>
2022-04-17 00:45:32 +0300

    Update Food class. integrate for room

--- app/src/main/java/com/herdal/foodsbook/model/Food.kt
+++ app/src/main/java/com/herdal/foodsbook/model/Food.kt
@@ -1,20 +1,31 @@
 package com.herdal.foodsbook.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-
+@Entity
 data class Food(
+    @ColumnInfo(name = "isim")
     @SerializedName("isim")
     val name: String?,
+    @ColumnInfo(name = "kalori")
     @SerializedName("kalori")
     val calorie: String?,
+    @ColumnInfo(name = "first_name")
     @SerializedName("karbonhidrat")
     val carbohydrate: String?,
+    @ColumnInfo(name = "protein")
     @SerializedName("protein")
     val protein: String?,
+    @ColumnInfo(name = "yag")
     @SerializedName("yag")
     val fat: String?,
+    @ColumnInfo(name = "gorsel")
     @SerializedName("gorsel")
     val image: String?
     ) {
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0
 }
