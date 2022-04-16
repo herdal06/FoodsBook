c06f7e884a7e848cc8c0fc963fb306e9eb8b741c
herdal06 <h.erdal721@gmail.com>
2022-04-16 05:42:21 +0300

    Add Food data class

---
+++ app/src/main/java/com/herdal/foodsbook/model/Food.kt
@@ -0,0 +1,11 @@
+package com.herdal.foodsbook.model
+
+
+data class Food(
+    val id: Int?,
+    val name: String?,
+    val calorie: String?,
+    val carbohydrate: String?,
+    val protein: String?,
+    val fat: String?) {
+}
