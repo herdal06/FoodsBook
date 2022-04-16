53e4476f3fb58e3d89a21d7b9cd8d5c450e653ef
herdal06 <h.erdal721@gmail.com>
2022-04-16 17:26:15 +0300

    Update Food class.

--- app/src/main/java/com/herdal/foodsbook/viewmodel/FoodListViewModel.kt
+++ app/src/main/java/com/herdal/foodsbook/viewmodel/FoodListViewModel.kt
@@ -8,4 +8,15 @@ class FoodListViewModel : ViewModel() {
     val foods = MutableLiveData<List<Food>>()
     val errorMessage = MutableLiveData<Boolean>()
     val loadingBar = MutableLiveData<Boolean>()
+
+    fun refreshData() {
+        val apple = Food("apple","100","10","5","1","image")
+        val strawberry = Food("strawberry","100","10","5","1","image")
+        val watermelon = Food("watermelon","100","10","5","1","image")
+
+        val foodList = arrayListOf<Food>(apple,strawberry,watermelon)
+        foods.value = foodList
+        errorMessage.value = false
+        loadingBar.value = false
+    }
 }
