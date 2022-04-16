77feb44fe355c793dc5db003c3bc268c3a024c07
herdal06 <h.erdal721@gmail.com>
2022-04-16 05:55:01 +0300

    Add adapter for recyclerView. Update misspelled variable name

---
+++ app/src/main/java/com/herdal/foodsbook/adapter/FoodAdapter.kt
@@ -0,0 +1,37 @@
+package com.herdal.foodsbook.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.herdal.foodsbook.R
+import com.herdal.foodsbook.model.Food
+import kotlinx.android.synthetic.main.food_recycler_row.view.*
+
+class FoodAdapter(private val foodList: ArrayList<Food>) : RecyclerView.Adapter<FoodAdapter.RecyclerViewHolder>() {
+    inner class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.food_recycler_row,parent,false)
+        return RecyclerViewHolder(inflater)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        holder.itemView.foodName.text = foodList.get(position).name
+        holder.itemView.foodCalorie.text = foodList.get(position).calorie
+        // todo : add image
+    }
+
+    override fun getItemCount(): Int {
+        return foodList.size
+    }
+
+    fun updateFoodList(newFoodList: List<Food>) {
+        foodList.clear()
+        foodList.addAll(newFoodList)
+        notifyDataSetChanged()
+    }
+}
